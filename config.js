4b6c868f15d989a5d41d6a185c7fa6064a2461fd
Levian Code <94874820+LevianCode1@users.noreply.github.com>
2021-12-31 05:08:30 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+	website: {
+		protocol: 'https://', // https:// or http://
+		domain: 'unruly-thinkable-salesman.glitch.me', // Site Linkiniz HTTPS OLMADAN VE / OLMADAN
+		port: 3000,
+    cookieSecret: 'N3.B4K1Y0R2UN.83N.L3V14N' //Çerez Secret Bunu Silmeyin
+	},
+  bot: {
+    name: 'Dark Giveaway', // Bot Isimi
+    token: 'OTE2NDY4OTk4OTMyMTU2NDM3.YaqmRA.DbBkXrFyEUga6XDZAwP5UR22hro',
+    logo: 'https://cdn.discordapp.com/attachments/897944618468007966/924459820273270794/LevianLogo1.Png', //BOT LOGO VE FAVICON
+    description: 'Sunucunuzda Rol Ve Sunucu Şartı İle Çekiliş Yapa Bilirsiniz', //BOT ACIKLAMASI
+    support: 'https://discord.gg/3Ke3XY3mJJ', // DESTEK SUNUCUSU
+    invite: 'https://discord.com/api/oauth2/authorize?client_id=916468998932156437&permissions=8&scope=bot', //DAVET LINKI
+    botlist: 'https://top.gg/tr/bot/892504741395963964', // TOP GG SITESI VEYA DIGER BOTLIST SITELERI
+    feature1: {
+      main: '! Nivadoria 🎄#4553', // ÖZELLİK
+      description: 'Çok Harika Bot ' // ÖZELLİK AÇIKLAMASI
+    },
+    feature2: {
+      main: 'Discordun Sahibiyim Böyle Bot Görmedim', // // ÖZELLİK AÇIKLAMASI
+      description: 'Jason Citron' // ÖZELLİK
+    }
+      },
+  img: {
+    img1: 'https://cdn.discordapp.com/attachments/913622936458506240/925507616266919966/unknown.png', // YARDIM MENÜSÜ SS 
+    img2: 'https://cdn.discordapp.com/attachments/913622936458506240/925508477772763157/unknown.png' //HER HANGI BIR KOMUT SS
+}
+  
+};
---
+++ index.js
@@ -0,0 +1,138 @@
+//--------MODULLER----------\\
+const url = require('url'),
+	path = require('path'),
+	express = require('express'),
+	session = require('express-session'),
+	ejs = require('ejs'),
+	fetch = require('node-fetch'),
+	config = require('./config'),
+	bodyParser = require('body-parser'),
+	app = express(),
+	MemoryStore = require('memorystore')(session),
+	dataDir = path.resolve(`${process.cwd()}${path.sep}website`),
+	templateDir = path.resolve(`${dataDir}${path.sep}pages`);
+	
+
+
+
+	//------------BASLAT------------\\
+    console.log(
+		`[INFO]: SITEYI BASLATMAYI DENIYORUZ`
+	);
+	console.log('-------------------------------------');
+
+    try {
+		const domainUrl = new URL(config.website.protocol + config.website.domain);
+		domain = {
+			host: domainUrl.hostname,
+			protocol: domainUrl.protocol
+		};
+	} catch (e) {
+		console.log(e);
+		throw new TypeError(
+			'[HATA]: DOMAIN HATALI '
+		);
+	}
+
+
+    app.use(
+		session({
+			store: new MemoryStore({ checkPeriod: 86400000 }),
+			secret:
+				config.website.cookieSecret,
+			resave: false,
+			saveUninitialized: false
+		})
+	);
+
+    app.use(bodyParser.json());
+	app.use(
+		bodyParser.urlencoded({
+			extended: true
+		})
+	);
+
+    app.engine('html', ejs.renderFile);
+	app.set('view engine', 'html');
+
+    app.use('/', express.static(path.resolve(`${dataDir}${path.sep}/`)));
+
+    
+
+    //------------------FUNCTION-----------\\
+	function render(res, req, template, data = {}) {
+		const baseData = {
+			logo: config.bot.logo,
+			name: config.bot.name,
+			description: config.bot.description,
+      img1: config.img.img1,
+      img2: config.img.img2,
+      feature1: config.bot.feature1.main,
+      feature1d: config.bot.feature1.description,
+      feature2d: config.bot.feature2.main,
+      feature2: config.bot.feature2.description,
+		};
+		res.render(
+			path.resolve(`${templateDir}${path.sep}${template}`),
+			Object.assign(baseData, data)
+		);
+	}
+
+    //----------RENDER PAGES-----------\\
+if (config.showStats) {
+  const Discord = require('discord.js')
+  const client = new Discord.Client({
+    shards: "auto",
+   
+    allowedMentions: {
+      parse: [ ],
+      repliedUser: false,
+    },
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+   intents: [],
+});
+
+
+  client.login(config.bot.token)
+
+} else {
+    app.get('/', (req, res) => {
+		render(res, req, 'normal-index.ejs');
+	});
+}
+
+
+
+
+
+	app.get('/discord', (req, res) => {
+		res.redirect(config.bot.support)
+	})
+
+  app.get('/dc', (req, res) => {
+		res.redirect(config.bot.support)
+	})
+
+	app.get('/invite', (req, res) => {
+
+		res.redirect(config.bot.invite)
+	})
+
+  app.get('/vote', (req, res) => {
+
+		res.redirect(config.bot.botlist)
+	})
+
+app.use(function(req, res) {
+  res.status(404).sendFile(__dirname + '/website/pages/404.html');
+
+	});
+
+    //---------END----------\\
+    app.listen(config.website.port, null, null, () =>
+		console.log(`LEVIAN#0186`)
+	);
+
+	let domainUrl = config.website.protocol + config.website.domain
+
+    console.log(`[INFO]: Website is now live on ${domainUrl}`)
